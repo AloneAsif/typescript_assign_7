26cfdeece24f6c19cd5ce69b2879ec32b50a8245
[AloneAsif] <[asifleon3@gmail.com]>
2024-03-09 14:17:27 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+// Question no 7 and 7    Number Eight:
+console.log(`addition ${4 + 4}`); // output will be 8
+console.log(`substraction ${16 - 8}`);
+console.log(`Multiplication ${4 * 2}`);
+console.log(`divide ${16 / 2}`);
